06185d13b4d5ea96f87b479b0be05deb356fb6c1
Melissa Longenberger <242424+melissa-24@users.noreply.github.com>
2020-04-22 23:12:44 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export default [
     name: "Waves & Stripes",
     sku: "AF01",
     id: 0,
-    price: 45.0,
+    price: "20 - $50,
     imageUrl: "http://images.craftsnherbs.com/crochet/afghan01.jpg",
     description: "Can be made in sizes baby - Queen.  You can chose the colors",
     shipping:
@@ -13,10 +13,20 @@ export default [
     name: "Gentle Waves",
     sku: "AF02",
     id: 1,
-    price: 45.0,
+    price: "20 - $50",
     imageUrl: "http://images.craftsnherbs.com/crochet/afghan04.jpg",
     description: "Soft waves in a mandela yarns make this afghan unique",
     shipping:
-      "All orders are made to order. Shipping time will be discussed through email."
+      "All orders are made to order.  The time to make the product will depend on the size you order.  Once completed, it will be shipped."
   }
+  {
+    name: "Little Animals",
+    sku: "AML01",
+    id: 2,
+    price: 10,
+    imageUrl: "http://images.craftsnherbs.com/crochet/animal01.jpg",
+    description: "These little guys can fit into the palm of your hand.  Cute fun and full of personality.  You chose the animal, and colors",
+    shipping:       "All orders are made to order.  The time to make the product will depend on the size you order.  Once completed, it will be shipped."
+
+  },
 ];
