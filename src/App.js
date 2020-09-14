350d19600a61b62af83c013a4ff267f1fddda8f6
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-09-13 20:51:33 -0400

    pushing unsaved changes

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ import Contact from "./components/Contact";
 import ShoppingCart from "./components/ShoppingCart";
 
 export default function App() {
-  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-      var s1=document.createElement("script"),
-        s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/5c04089140105007f37aa009/default';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-      })();
+  // var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  //   (function(){
+  //     var s1=document.createElement("script"),
+  //       s0=document.getElementsByTagName("script")[0];
+  //       s1.async=true;
+  //       s1.src='https://embed.tawk.to/5c04089140105007f37aa009/default';
+  //       s1.charset='UTF-8';
+  //       s1.setAttribute('crossorigin','*');
+  //       s0.parentNode.insertBefore(s1,s0);
+  //     })();
   const [products, setProducts] = useState(data);
   return (
     <div className="App">
